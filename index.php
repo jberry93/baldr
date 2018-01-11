034cd1e79a9821e236192e9a09bc50be57ac2ab7
jberry93 <jeffreyberry93@gmail.com>
2018-01-10 16:15:04 -0800

    refactor(pieces): added 2 pieces; navbar and sidebar. Need to get search and comments working

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
 $context = Timber::get_context();
-$context['dynamic_sidebar'] = Timber::get_widgets('dynamic_sidebar');
 
 Timber::render('index.twig', $context);
--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,27 @@
 import '../scss/app.scss';
 
-class HelloWorld {
+class BaldrApp {
   constructor() {
-    console.info('HelloWorld class');
+    document.addEventListener('DOMContentLoaded', this.initNavbar);
+  }
+
+  initNavbar() {
+    const $burgers = Array.prototype.slice.call(
+      document.querySelectorAll('.navbar-burger'),
+      0
+    );
+    if ($burgers.length > 0) {
+      $burgers.forEach($el => {
+        $el.addEventListener('click', () => {
+          let target = $el.dataset.target;
+          let $target = document.getElementById(target);
+
+          $el.classList.toggle('is-active');
+          $target.classList.toggle('is-active');
+        });
+      });
+    }
   }
 }
 
-const hi = new HelloWorld();
+new BaldrApp();
